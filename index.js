93021fd967be939a4b7a379d37524b874ed80c13
thuyanduong-flatiron <thuyanannduong@gmail.com>
2019-04-23 20:19:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(array, name){
+  return array.filter(element => element.toLocaleLowerCase() === name.toLocaleLowerCase())
+}
+
+function fuzzyMatch(array, name){
+  return array.filter(element => element.indexOf(name) === 0)
+}
+
+function matchName(array, name){
+  return array.filter(obj => obj.name === name)
+
+}
